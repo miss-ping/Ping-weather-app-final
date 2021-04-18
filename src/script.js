a597e76a466cfb7a5930ea647934e65438ad0e9e
miss-ping <79873852+miss-ping@users.noreply.github.com>
2021-04-18 09:54:00 +0100

    Display temperature

--- src/script.js
+++ src/script.js
@@ -1,5 +1,16 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
+  let dateElement = document.querySelector("#date");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  countryElement.innerHTML = response.data.sys.country;
+  humidityElement.innerHTML = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "51c757e95cb1fdd4bc45100437afbefb";
