493bb0bccf7e49b73828c1071f9147f9a82cc459
miss-ping <79873852+miss-ping@users.noreply.github.com>
2021-04-18 09:08:46 +0100

    Added api key and url

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  console.log(response.data.main.temp);
 }
 
 let apiKey = "51c757e95cb1fdd4bc45100437afbefb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
